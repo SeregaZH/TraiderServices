8914521dff4a0753c2e4bd40765795a76661e6b0
SeregaZH <zhelezko1990@gmail.com>
2014-08-03 16:33:54 +0300

    initialize superuser

--- TraiderInformationService/TraiderInformationService.Database/Schema Objects/Schemas/dbo/Tables/User.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[User]
-(
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
-    [UpdatedBy] varchar(200) NOT NULL, 
-    [UpdatedDate] DATETIME NOT NULL, 
-    CONSTRAINT [FK_Users_ToUserInfo] FOREIGN KEY (Id) REFERENCES [UserInfo](Id) ON DELETE CASCADE,     
-)
